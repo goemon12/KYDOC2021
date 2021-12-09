0734502ea1d308cd512d1f0aa02e94be08aafb23
Tadahiro Kato <goemon12.tc4@gmail.com>
2021-12-09 23:51:09 +0900

    Initial Commit

---
+++ KYDOC2021/KYDOC2021App.swift
@@ -0,0 +1,17 @@
+//
+//  KYDOC2021App.swift
+//  KYDOC2021
+//
+//  Created by Tadahiro Kato on 2021/12/09.
+//
+
+import SwiftUI
+
+@main
+struct KYDOC2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KYDOC2021UITests/KYDOC2021UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KYDOC2021UITestsLaunchTests.swift
+//  KYDOC2021UITests
+//
+//  Created by Tadahiro Kato on 2021/12/09.
+//
+
+import XCTest
+
+class KYDOC2021UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
